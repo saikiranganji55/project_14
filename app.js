f5562b603ccdf7f9cd4866ce5f8d5946ec48e410
SaiKiran <you@example.com>
2023-05-27 13:49:46 +0000

    Tests for Gadgets Page

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+module.exports = app;
+
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
